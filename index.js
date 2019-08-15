d8af8b0d7429c4c94491e7d968591133c4b63ef2
betsybaileyy <betsy.bailey80@gmail.com>
2019-08-15 15:02:21 -0700

    returning json working

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const request = require('request');
 
 let apiKey = '75e61380b673bbfebc3779698520ce1b';
 let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
 
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+      let weather = JSON.parse(body)
+
+      let message = `It's ${weather.main.temp} degrees in
+                     ${weather.name}!`;
+      console.log(message);
   }
 });
