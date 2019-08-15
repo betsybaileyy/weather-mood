d51ab8bfa4a09dd71f7ffb62a2695eb28a774a5d
betsybaileyy <betsy.bailey80@gmail.com>
2019-08-15 14:53:30 -0700

    api

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const request = require('request');
+
+let apiKey = '75e61380b673bbfebc3779698520ce1b';
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
