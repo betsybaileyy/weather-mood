41bed5bb9f30520e2d52872fceaa8c832a38eab9
betsybaileyy <betsy.bailey80@gmail.com>
2019-08-15 16:04:54 -0700

    body parser working

--- index.js
+++ index.js
@@ -2,27 +2,14 @@ const request = require('request');
 const argv = require('yargs').argv;
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 
-let apiKey = '75e61380b673bbfebc3779698520ce1b';
-let city = argv.c || 'san francisco';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-
-request(url, function (err, response, body) {
-  if(err){
-    console.log('error:', error);
-  } else {
-      let weather = JSON.parse(body)
-
-      let message = `It's ${weather.main.temp} degrees in
-                     ${weather.name}!`;
-      console.log(message);
-  }
-});
 
 app.get('/', function (req, res) {
   res.render('index');
@@ -33,5 +20,21 @@ app.listen(3000, function () {
 })
 
 app.post('/', function (req, res) {
-  res.render('index');
+    let apiKey = '75e61380b673bbfebc3779698520ce1b';
+    let city = argv.c || 'san francisco';
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+
+    request(url, function (err, response, body) {
+      if(err){
+        console.log('error:', error);
+      } else {
+          let weather = JSON.parse(body)
+
+          let message = `It's ${weather.main.temp} degrees in
+                         ${weather.name}!`;
+          console.log(message);
+      }
+    });
+  console.log(req.body.city);
 })
